b555ff21b46ea5aceb464faf9b8d23cafee96b49
Prabhu Teja Dasari <prabhuteja.dasari@pinion.services>
2023-10-04 17:45:47 +0530

    icons

--- src/screens/Employee/View-Employee.tsx
+++ src/screens/Employee/View-Employee.tsx
@@ -124,7 +124,13 @@ function ViewEmployee(){
         <p className='addname-p'>Duplicate</p>
         </div>
         <div className='sub-section-container5'>
-        <p className='color-p'>Add or Remove Direct Reports</p>
+        <p className='color-p'> <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-notes" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#62b2b5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M5 3m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" />
+  <path d="M9 7l6 0" />
+  <path d="M9 11l6 0" />
+  <path d="M9 15l4 0" />
+</svg> Manage Work Address</p>
         </div>
         </div>
         </div>
