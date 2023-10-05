4c5e1031aba5dcdfa853fd18ade473ebf2a65b8a
Prabhu Teja Dasari <prabhuteja.dasari@pinion.services>
2023-10-05 17:52:48 +0530

    toggle button changed

--- src/components/common/Elements/ToggleButton.tsx
+++ src/components/common/Elements/ToggleButton.tsx
@@ -6,36 +6,47 @@ import {
   useMantineColorScheme
 } from '@mantine/core'
 import { IconMoon, IconSun } from '@tabler/icons'
+import { ActionIcon } from '@mantine/core';
+import {  IconMoonStars } from '@tabler/icons';
+
 
 export default function SegmentedToggle() {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
-
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
   return (
-    <Group position="center" my="xl">
-      <SegmentedControl
-        value={colorScheme}
-        onChange={(value: 'light' | 'dark') => toggleColorScheme(value)}
-        data={[
-          {
-            value: 'light',
-            label: (
-              <Center>
-                <IconSun size={16} stroke={1.5} />
-                <Box ml={10}>Light</Box>
-              </Center>
-            )
-          },
-          {
-            value: 'dark',
-            label: (
-              <Center>
-                <IconMoon size={16} stroke={1.5} />
-                <Box ml={10}>Dark</Box>
-              </Center>
-            )
-          }
-        ]}
-      />
-    </Group>
+    // <Group position="center" my="xl">
+    //   <SegmentedControl
+    //     value={colorScheme}
+    //     onChange={(value: 'light' | 'dark') => toggleColorScheme(value)}
+    //     data={[
+    //       {
+    //         value: 'light',
+    //         label: (
+    //           <Center>
+    //             <IconSun size={16} stroke={1.5} />
+    //             <Box ml={10}>Light</Box>
+    //           </Center>
+    //         )
+    //       },
+    //       {
+    //         value: 'dark',
+    //         label: (
+    //           <Center>
+    //             <IconMoon size={16} stroke={1.5} />
+    //             <Box ml={10}>Dark</Box>
+    //           </Center>
+    //         )
+    //       }
+    //     ]}
+    //   />
+    // </Group>
+    <ActionIcon
+      variant="outline"
+      color={dark ? 'yellow' : 'blue'}
+      onClick={() => toggleColorScheme()}
+      title="Toggle color scheme"
+    >
+      {dark ? <IconSun size={18} /> : <IconMoonStars size={18} />}
+    </ActionIcon>
   )
 }
